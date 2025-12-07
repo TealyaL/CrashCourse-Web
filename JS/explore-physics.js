479732d28e146462ca17de14378e109e471a70aa
Tea <xanhlamtv@gmail.com>
2025-12-07 11:33:25 +0700

    json & js file added

---
+++ JS/explore-physics.js
@@ -0,0 +1,24 @@
+const fetchData = async () =>{
+    let response = await fetch("./data.json");
+    let allData = await response.json();
+    
+    data = allData.filter(item => item.category === "Physical Science");
+
+    let allCourseList = document.querySelector("#course-list");
+    allCourseList.innerHTML = data.map(item =>{
+        return `
+        <div class="card col-lg-3 col-md-6">
+            <img src="${item.image}" class="card-img-top" alt="${item.title}">
+            <div class="card-body">
+                <a href="${item.page}" class="card-title">
+                    <h5>${item.title}</h5>
+                </a>
+                <p class="card-text">${item.description}</p>
+                <a href="${item.page}" class="btn btn-warning">View</a>
+            </div>
+        </div>
+        `
+    }).join("");
+}
+
+fetchData()
